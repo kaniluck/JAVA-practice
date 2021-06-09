7a6423a2eda7d1f6e746628c1784983bbe9fdc9c
kaniluck <takahashikeisuke.0514@gmail.com>
2021-06-09 17:24:12 +0900

    Chapter04

---
+++ src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+    public static void main(String[] args) {
+        final int LUCKY_NUMBER = 5;
+        String str = "高橋　髙橋";
+        System.out.println(str);
+        str = "鶏口となるも牛後となるなかれ";
+        System.out.println(str);
+        System.out.println(LUCKY_NUMBER);
+    }
+}
